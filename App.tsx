53f04ebb9e6e630a3b660dc2aca95ddc02487b00
JoelFrancisco <joelfranciscofilho@gmail.com>
2024-02-25 15:50:33 -0300

    adding prettier

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
-import { Camera, CameraType } from 'expo-camera';
-import { useState } from 'react';
-import { Button, Text, TouchableOpacity, View } from 'react-native';
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import { Button, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
   const [type, setType] = useState(CameraType.back);
@@ -13,21 +13,28 @@ export default function App() {
   if (!permission.granted) {
     return (
       <View className="flex-1 justify-center">
-        <Text className="text-center">We need your permission to show the camera</Text>
+        <Text className="text-center">
+          We need your permission to show the camera
+        </Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
   }
 
   function toggleCameraType() {
-    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back,
+    );
   }
 
   return (
     <View className="flex-1 justify-center">
       <Camera className="flex-1" type={type}>
         <View className="flex-1 flex-row bg-transparent m-[64]">
-          <TouchableOpacity className="flex-1 items-center self-end" onPress={toggleCameraType}>
+          <TouchableOpacity
+            className="flex-1 items-center self-end"
+            onPress={toggleCameraType}
+          >
             <Text className="font-bold text-lg text-white">Flip Camera</Text>
           </TouchableOpacity>
         </View>
