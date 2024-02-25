ce51c6d8aa0f2860ac7c5bd6d57d0beb95d88cc7
JoelFrancisco <joelfranciscofilho@gmail.com>
2024-02-25 15:17:38 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Camera, CameraType } from 'expo-camera';
+import { useState } from 'react';
+import { Button, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    return (
+      <View className="flex-1 justify-center">
+        <Text className="text-center">We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType(current => (current === CameraType.back ? CameraType.front : CameraType.back));
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 justify-center">
+      <Camera className="flex-1" type={type}>
+        <View className="flex-1 flex-row bg-transparent m-[64]">
+          <TouchableOpacity className="flex-1 items-center self-end" onPress={toggleCameraType}>
+            <Text className="font-bold text-lg text-white">Flip Camera</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
