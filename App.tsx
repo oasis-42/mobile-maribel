1e656d2946127cb48f604edba5fbad07f3b39268
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-04-13 19:13:29 -0300

    feat: Finishing the visual components of Onboarding 1

--- App.tsx
+++ App.tsx
@@ -1,43 +1,90 @@
-import { Camera, CameraType } from "expo-camera";
-import { useState } from "react";
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import { Appbar, Card, PaperProvider } from "react-native-paper";
+import { Image, Pressable, ScrollView, Text, View } from "react-native";
 
 export default function App() {
-  const [type, setType] = useState(CameraType.back);
-  const [permission, requestPermission] = Camera.useCameraPermissions();
-
-  if (!permission) {
-    return <View />;
-  }
-
-  if (!permission.granted) {
-    return (
-      <View>
-        <Text>
-          We need your permission to show the camera
-        </Text>
-        <Button onPress={requestPermission} title="grant permission" />
-      </View>
-    );
-  }
-
-  function toggleCameraType() {
-    setType((current) =>
-      current === CameraType.back ? CameraType.front : CameraType.back,
-    );
-  }
-
   return (
-    <View>
-      <Camera type={type}>
-        <View>
-          <TouchableOpacity
-            onPress={toggleCameraType}
-          >
-            <Text>Flip Camera</Text>
-          </TouchableOpacity>
+    <PaperProvider>
+      <Appbar.Header
+        style={{
+          backgroundColor: "#D7D7D7",
+          gap: 24,
+        }}
+      >
+        <Appbar.Action icon="arrow-left" onPress={() => {}} />
+        <Appbar.Content title="Etapa 1 de 3" />
+      </Appbar.Header>
+      <ScrollView style={{ flexDirection: "column" }}>
+        <Card
+          style={{
+            padding: 4,
+            backgroundColor: "transparent",
+            margin: 12,
+            shadowColor: "transparent",
+          }}
+        >
+          <Card.Content>
+            <Text style={{ fontSize: 25, fontWeight: "700", color: "#2E3E4B" }}>
+              Avalie suas redações
+            </Text>
+            <Text
+              style={{
+                fontSize: 16,
+                fontWeight: "600",
+                color: "#2E3E4B",
+                textAlign: "justify",
+                lineHeight: 24,
+              }}
+            >
+              Faça as correções de suas redações do ENEM através da Maribel e
+              tenha orientações imediatas.
+            </Text>
+          </Card.Content>
+        </Card>
+        <View style={{ alignItems: "center" }}>
+          <Image source={require("./assets/processImg.png")} style={{}} />
         </View>
-      </Camera>
-    </View>
+        <View
+          style={{
+            marginTop: 12,
+            gap: 14,
+            padding: 10,
+          }}
+        >
+          <Pressable
+            onPress={() => alert("Hello!")}
+            style={{
+              backgroundColor: "#044884",
+              width: "auto",
+              height: 56,
+              flex: 1,
+              justifyContent: "center",
+              padding: 8,
+            }}
+          >
+            <Text
+              style={{
+                color: "#FFFFFF",
+                fontWeight: "600",
+                textAlign: "center",
+              }}
+            >
+              Continuar
+            </Text>
+          </Pressable>
+          <Pressable
+            onPress={() => alert("Ola!")}
+            style={{
+              flex: 1,
+              alignItems: "center",
+              padding: 8,
+            }}
+          >
+            <Text style={{ fontWeight: "600", textAlign: "center" }}>
+              Pular
+            </Text>
+          </Pressable>
+        </View>
+      </ScrollView>
+    </PaperProvider>
   );
 }
