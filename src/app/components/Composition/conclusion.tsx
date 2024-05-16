0fdf60f52701a2bdc82e9c88b2a021fdfda082e8
NicolasLK <nicolas.kaminski.2001@gmail.com>
2024-05-16 20:53:37 -0300

    style: Modifing the area texts

--- src/app/components/Composition/conclusion.tsx
+++ src/app/components/Composition/conclusion.tsx
@@ -1,12 +1,17 @@
 import { Pressable, ScrollView, Text, TextInput, View } from "react-native";
 
-export function Conclusion({ setCompositionStage }: CompositionStageProps) {
+export function Conclusion({
+  essayData,
+  setCompositionStage,
+}: CompositionStageProps) {
   return (
     <>
       <ScrollView
         style={{
           flexDirection: "column",
           borderWidth: 1,
+          borderRadius: 5,
+          borderColor: "#D7D7D7",
           paddingHorizontal: 8,
           paddingVertical: 14,
           backgroundColor: "white",
@@ -16,18 +21,7 @@ export function Conclusion({ setCompositionStage }: CompositionStageProps) {
         <Text style={{ fontWeight: "600", fontSize: 16, marginBottom: 12 }}>
           Conclusão
         </Text>
-        <Text>
-          Figma ipsum component variant main layer. Strikethrough align content
-          line figjam shadow comment create move. Thumbnail flatten polygon
-          bullet edit outline vector object clip asset. Layout union underline
-          pencil link flatten list. Comment selection frame italic arrange main
-          text. Ipsum export auto flatten share. Team figma invite ipsum
-          horizontal. Figma rectangle subtract pencil ipsum layout ipsum editor
-          component horizontal. Align inspect edit bold ipsum draft boolean.
-          Opacity draft team duplicate slice device. Underline image boolean
-          ellipse library. Mask outline group stroke thumbnail community
-          subtract strikethrough line. Frame ellipse pixel thumbnail font.
-        </Text>
+        <Text>{essayData}</Text>
       </ScrollView>
       <View
         style={{
@@ -35,8 +29,11 @@ export function Conclusion({ setCompositionStage }: CompositionStageProps) {
           justifyContent: "space-between",
           alignItems: "center",
           borderWidth: 1,
+          borderRadius: 5,
+          borderColor: "#D7D7D7",
           paddingHorizontal: 8,
           paddingVertical: 14,
+          backgroundColor: "white",
         }}
       >
         <Pressable onPress={() => setCompositionStage("development")}>
--- src/app/screens/onboardings/evaluatedComposition.tsx
+++ src/app/screens/onboardings/evaluatedComposition.tsx
@@ -10,12 +10,13 @@ export default function EvaluatedComposition() {
     "introduction" | "development" | "conclusion"
   >("introduction");
 
-  const fechtData = () => {
-    fetch(
-      "https://schema.getpostman.com/json/collection/v2.1.0/collection.json"
-    ).then((response) => {
-      return response.json();
-    });
+  const essay = {
+    introduction:
+      "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
+    development:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
+    conclusion:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
   };
 
   return (
@@ -31,6 +32,8 @@ export default function EvaluatedComposition() {
             paddingVertical: 14,
             justifyContent: "space-between",
             borderWidth: 1,
+            borderRadius: 5,
+            borderColor: "#D7D7D7",
             backgroundColor: "white",
           }}
         >
@@ -38,21 +41,31 @@ export default function EvaluatedComposition() {
           <Text style={{ fontWeight: "600", fontSize: 16 }}>678 pontos</Text>
         </View>
         {compositionStage === "introduction" && (
-          <Introduction setCompositionStage={setCompositionStage} />
+          <Introduction
+            essayData={essay.introduction}
+            setCompositionStage={setCompositionStage}
+          />
         )}
         {compositionStage === "development" && (
-          <Development setCompositionStage={setCompositionStage} />
+          <Development
+            essayData={essay.development}
+            setCompositionStage={setCompositionStage}
+          />
         )}
         {compositionStage === "conclusion" && (
-          <Conclusion setCompositionStage={setCompositionStage} />
+          <Conclusion
+            essayData={essay.conclusion}
+            setCompositionStage={setCompositionStage}
+          />
         )}
         <View style={{ flexDirection: "column", gap: 14 }}>
           <Pressable
             style={{
               backgroundColor: "#044884",
               justifyContent: "center",
-              padding: 12,
               borderRadius: 8,
+              padding: 8,
+              height: 56,
             }}
           >
             <Text
@@ -69,8 +82,9 @@ export default function EvaluatedComposition() {
             style={{
               backgroundColor: "#044884",
               justifyContent: "center",
-              padding: 12,
               borderRadius: 8,
+              padding: 8,
+              height: 56,
             }}
           >
             <Text
